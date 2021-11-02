591bfd7e0148f32657f29cf2f61342fcbf3a7082
Debnandini Mukherjee <debnandini.mukherjee@gmail.com>
2021-11-01 20:01:27 -0500

    adding new pysearch.pyx

--- working_dir/pysearch.pyx
+++ working_dir/pysearch.pyx
@@ -1,11 +1,9 @@
 cdef extern from "search.h":
-	int main(int argc, char *argv[]):
-		segvec=[]
-		segvec.resize(argc)
-		segvec[0]=0
-		segvec[1]=argv[1]
-		segvec[2]=argv[2]
-		main(argc, segvec)
+	int main(int argc, char *argv[])
 
-def py_main(int argc, list segvec):
+def py_main(int argc, list argv):
+	cdef vector[char] segvec
+	segvec.resize(argc)
+	segvec[0]=0
+	segvec[1]=argv[1]
 	main(argc, segvec)
