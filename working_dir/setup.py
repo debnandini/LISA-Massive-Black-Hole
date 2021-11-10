ac226d72988a8b88272e090c881fc59791c3a441
Debnandini Mukherjee <debnandini.mukherjee@gmail.com>
2021-11-10 14:14:27 -0600

    changed search.c and related (adding setup)

--- working_dir/setup.py
+++ working_dir/setup.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
 search_extension = Extension(
         name="pysearch",
-	sources=["pysearch.pyx"],
-	libraries=["search"],
+	sources=["pysearch.pyx","IMRPhenomD_internals.c","IMRPhenomD.c"],
+	libraries=["search","IMRPhenomD_internals","IMRPhenomD"],
 	library_dirs=["lib"],
 	include_dirs=["lib"]
 )
